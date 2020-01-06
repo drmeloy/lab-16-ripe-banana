811e3d84aac298e04f001d5c73bdea97317c4862
drmeloy <drmeloy@gmail.com>
2020-01-06 13:11:00 -0700

    Studio model and routes done

---
+++ __tests__/Studio-model.test.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Studio = require('../lib/models/Studio');
+
+describe('Studio model', () => {
+  it('requires a name', () => {
+    const studio = new Studio({
+      address: {
+        city: 'Boise',
+        state: 'Idaho',
+        country: 'USA'
+      }
+    });
+
+    const { errors } = studio.validateSync();
+
+    expect(errors.name.message).toEqual('Path `name` is required.');
+  });
+
+  
+});
---
+++ __tests__/studios-route.test.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Studio = require('../lib/models/Studio');
+
+describe('studios routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  
+  let studio;
+  beforeEach(async() => {
+    studio = await Studio.create({
+      name: 'Boise Studios',
+      address: {
+        city: 'Boise',
+        state: 'Idaho',
+        country: 'USA'
+      }
+    });
+  });
+
+  it('gets all studios', () => {
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: studio.id,
+          name: 'Boise Studios',
+          address: {
+            city: 'Boise',
+            state: 'Idaho',
+            country: 'USA'
+          },
+          __v: 0
+        }]);
+      });
+  });
+
+  it('gets a studio by id', () => {
+    return request(app)
+      .get(`/api/v1/studios/${studio.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: studio.id,
+          name: 'Boise Studios',
+          address: {
+            city: 'Boise',
+            state: 'Idaho',
+            country: 'USA'
+          },
+          __v: 0
+        });
+      });
+  });
+});
