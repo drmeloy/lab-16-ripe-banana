65ce9881ac563c0bc706070dccd9015ccda90bf2
drmeloy <drmeloy@gmail.com>
2020-01-06 16:17:03 -0700

    added rating validation to Review model

--- __tests__/model tests/Review-model.test.js
+++ __tests__/model tests/Review-model.test.js
@@ -31,6 +31,32 @@ describe('Review model', () => {
     expect(errors.rating.message).toEqual('Path `rating` is required.');
   });
 
+  it('rating has max of 5', () => {
+    const review = new Review({
+      rating: 6,
+      reviewer: reviewer.id,
+      review: 'It was the most meaningful movie I have ever had the privilege to see.',
+      film: film.id
+    });
+
+    const { errors } = review.validateSync();
+
+    expect(errors.rating.message).toEqual('Path `rating` (6) is more than maximum allowed value (5).');
+  });
+
+  it('rating has min of 1', () => {
+    const review = new Review({
+      rating: 0,
+      reviewer: reviewer.id,
+      review: 'It was the most meaningful movie I have ever had the privilege to see.',
+      film: film.id
+    });
+
+    const { errors } = review.validateSync();
+
+    expect(errors.rating.message).toEqual('Path `rating` (0) is less than minimum allowed value (1).');
+  });
+
   it('requires a reviewer', () => {
     const review = new Review({
       rating: 5,
