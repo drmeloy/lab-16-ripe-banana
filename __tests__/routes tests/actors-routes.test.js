8e2133d2f04b5c979dafe65398ad316a8aa05f73
drmeloy <drmeloy@gmail.com>
2020-01-08 12:07:12 -0700

    actors routes refactored

--- __tests__/routes tests/actors-routes.test.js
+++ __tests__/routes tests/actors-routes.test.js
@@ -1,31 +1,36 @@
+const { getActor, getActors, getFilms } = require('../../lib/helpers/data-helpers');
 const request = require('supertest');
 const app = require('../../lib/app');
 
 describe('actors routes', () => {
-  it('gets all actors', () => {
+  it('gets all actors', async() => {
+    const actors = await getActors();
+
     return request(app)
       .get('/api/v1/actors')
       .then(res => {
-        expect(res.body).toEqual([{
-          _id: actor.id,
-          name: 'Megaman'
-        }]);
+        expect(res.body).toHaveLength(actors.length);
+        actors.forEach(actor => {
+          delete actor.__v;
+          delete actor.pob;
+          delete actor.dob;
+          expect(res.body).toContainEqual(actor);
+        });
       });
   });
 
-  it('gets an actor by id', () => {
+  it('gets an actor by id', async() => {
+    const actor = await getActor();
+    await getFilms();
+
     return request(app)
-      .get(`/api/v1/actors/${actor.id}`)
+      .get(`/api/v1/actors/${actor._id}`)
       .then(res => {
         expect(res.body).toEqual({
-          name: 'Megaman',
-          dob: '1988-09-02T06:00:00.000Z',
-          pob: 'Boise',
-          films: [{
-            _id: film.id,
-            title: 'The Megaman Story',
-            released: 2015
-          }]
+          name: actor.name,
+          dob: actor.dob,
+          pob: actor.pob,
+          films: actor.films
         });
       });
   });
