fd40b7c95835da24f019db41874782aa39aa9c5f
drmeloy <drmeloy@gmail.com>
2020-01-08 12:36:26 -0700

    films routes: not yet done

--- __tests__/routes tests/films-routes.test.js
+++ __tests__/routes tests/films-routes.test.js
@@ -1,135 +1,65 @@
-require('dotenv').config();
-
+const { getFilm, getFilms, getStudio, getActor, getReviews } = require('../../lib/helpers/data-helpers');
 const request = require('supertest');
 const app = require('../../lib/app');
-const connect = require('../../lib/utils/connect');
-const mongoose = require('mongoose');
-const Film = require('../../lib/models/Film');
-const Studio = require('../../lib/models/Studio');
-const Actor = require('../../lib/models/Actor');
-const Review = require('../../lib/models/Review');
-const Reviewer = require('../../lib/models/Reviewer');
 
 describe('films routes', () => {
-  beforeAll(() => {
-    connect();
-  });
+  it('gets all films', async() => {
+    const films = await getFilms();
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
-  let film;
-  let studio;
-  let actor;
-  let review;
-  let reviewer;
-  beforeEach(async() => {
-    studio = await Studio.create({
-      name: 'Boise Studios'
-    });
-    
-    actor = await Actor.create({
-      name: 'Megaman'
-    });
-
-    film = await Film.create({
-      title: 'The Megaman Story',
-      studio: studio.id,
-      released: 2015,
-      cast: [{
-        role: 'Megaman',
-        actor: actor.id
-      }]
-    });
-
-    reviewer = await Reviewer.create({
-      name: 'Megaman',
-      company: 'Super Reviews'
-    });
-
-    review = await Review.create({
-      rating: 5,
-      reviewer: reviewer.id,
-      review: 'The most meaningful film ever made.',
-      film: film.id
-    });
-  });
-
-  it('gets all films', () => {
     return request(app)
       .get('/api/v1/films')
-      .then(films => {
-        expect(films.body).toEqual([{
-          _id: film.id,
-          title: 'The Megaman Story',
-          studio: {
-            _id: studio.id,
-            name: 'Boise Studios'
-          },
-          released: 2015
-        }]);
-      });
-  });
-
-  it('gets a film by id', () => {
-    return request(app)
-      .get(`/api/v1/films/${film.id}`)
       .then(res => {
-        expect(res.body).toEqual({
-          title: 'The Megaman Story',
-          studio: {
-            _id: studio.id,
-            name: 'Boise Studios'
-          },
-          released: 2015,
-          cast: [{
-            _id: expect.any(String),
-            role: 'Megaman',
-            actor: {
-              _id: actor.id,
-              name: 'Megaman'
-            }
-          }],
-          reviews: [{
-            _id: review.id,
-            rating: 5,
-            review: 'The most meaningful film ever made.',
-            reviewer: {
-              _id: reviewer.id,
-              name: 'Megaman'
-            }
-          }]
+        expect(res.body).toHaveLength(films.length);
+        films.forEach(film => {
+          
+          
+          expect(res.body).toContainEqual(film);
         });
       });
   });
 
-  it('can add a new film', () => {
+  it('gets a film by id', async() => {
+    const film = await getFilm();
+    await getReviews();
+
+    return request(app)
+      .get(`/api/v1/films/${film._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          title: film.title,
+          studio: film.studio,
+          released: film.released,
+          cast: film.cast,
+          reviews: film.reviews
+        });
+      });
+  });
+
+  it('can add a new film', async() => {
+    const studio = await getStudio();
+    const actor = await getActor();
+
     return request(app)
       .post('/api/v1/films')
       .send({
         title: 'Megaman Returns',
-        studio: studio.id,
+        studio: studio._id,
         released: 2016,
         cast: {
           role: 'Megaman',
-          actor: actor.id
+          actor: actor._id
         }
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           title: 'Megaman Returns',
-          studio: studio.id,
+          studio: studio._id,
           released: 2016,
           cast: [{
             _id: expect.any(String),
             role: 'Megaman',
-            actor: actor.id
+            actor: actor._id
           }],
           __v: 0
         });
