3bada48da81bc7aa9710b0066a3f07449d296cfd
drmeloy <drmeloy@gmail.com>
2020-01-09 14:10:25 -0700

    auth added to reviewer routes

--- __tests__/routes tests/reviewers-routes.test.js
+++ __tests__/routes tests/reviewers-routes.test.js
@@ -1,4 +1,4 @@
-const { getReviewer, getReviewers, getReviews, getReview } = require('../../lib/helpers/data-helpers');
+const { getReviewer, getReviewers, getReviews, getReview, getUserAgent, getAdminAgent } = require('../../lib/helpers/data-helpers');
 const Review = require('../../lib/models/Review');
 const request = require('supertest');
 const app = require('../../lib/app');
@@ -35,8 +35,23 @@ describe('reviewers routes', () => {
       });
   });
 
-  it('can create a new reviewer', () => {
+  it('requires a user to create a new reviewer', () => {
     return request(app)
+      .post('/api/v1/reviewers')
+      .send({
+        name: 'Protoman',
+        company: 'Portland Studios'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'jwt must be provided',
+          status: 500
+        });
+      });
+  });
+
+  it('can create a new reviewer', () => {
+    return getUserAgent()
       .post('/api/v1/reviewers')
       .send({
         name: 'Protoman',
@@ -52,10 +67,24 @@ describe('reviewers routes', () => {
       });
   });
 
-  it('can update a reviewer', async() => {
+  it('requires user to update a reviewer', async() => {
     const reviewer = await getReviewer();
 
     return request(app)
+      .patch(`/api/v1/reviewers/${reviewer._id}`)
+      .send({ name: 'Rock' })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'jwt must be provided',
+          status: 500
+        });
+      });
+  });
+
+  it('can update a reviewer', async() => {
+    const reviewer = await getReviewer();
+
+    return getUserAgent()
       .patch(`/api/v1/reviewers/${reviewer._id}`)
       .send({ name: 'Rock' })
       .then(res => {
@@ -68,11 +97,25 @@ describe('reviewers routes', () => {
       });
   });
 
-  it('can delete a reviewer with no reviews', async() => {
+  it('requires user to delete a reviewer', async() => {
     const emptyReviewer = await getReviewer();
     await Review.deleteMany({ reviewer: emptyReviewer._id });
     
     return request(app)
+      .delete(`/api/v1/reviewers/${emptyReviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'jwt must be provided',
+          status: 500
+        });
+      });
+  });
+
+  it('can delete a reviewer with no reviews', async() => {
+    const emptyReviewer = await getReviewer();
+    await Review.deleteMany({ reviewer: emptyReviewer._id });
+    
+    return getUserAgent()
       .delete(`/api/v1/reviewers/${emptyReviewer._id}`)
       .then(res => {
         expect(res.body).toEqual({
@@ -88,7 +131,7 @@ describe('reviewers routes', () => {
     const review = await getReview();
     const reviewer = await getReviewer({ _id: review.reviewer });
 
-    return request(app)
+    return getUserAgent()
       .delete(`/api/v1/reviewers/${reviewer._id}`)
       .then(res => {
         expect(res.status).toEqual(500);
